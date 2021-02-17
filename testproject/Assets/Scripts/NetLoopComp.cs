891d51880975fc601ee25fccd57a3f53f49c704f
M. Fatih MAR <mfatihmar@gmail.com>
2021-02-17 15:50:13 +0000

    add testbed

---
+++ testproject/Assets/Scripts/NetLoopComp.cs
@@ -0,0 +1,72 @@
+﻿using MLAPI;
+using UnityEngine;
+
+public class NetLoopComp : MonoBehaviour, INetworkUpdateSystem
+{
+    public void RegisterUpdates(int id)
+    {
+        switch (id % 5)
+        {
+            case 0:
+                this.RegisterAllNetworkUpdates();
+                break;
+            case 1:
+                this.RegisterNetworkUpdate(NetworkUpdateStage.FixedUpdate);
+                this.RegisterNetworkUpdate(NetworkUpdateStage.Update);
+                this.RegisterNetworkUpdate(NetworkUpdateStage.PreLateUpdate);
+                break;
+            case 2:
+                this.RegisterNetworkUpdate(NetworkUpdateStage.EarlyUpdate);
+                this.RegisterNetworkUpdate(NetworkUpdateStage.PreUpdate);
+                this.RegisterNetworkUpdate(NetworkUpdateStage.PostLateUpdate);
+                break;
+            case 3:
+                this.RegisterNetworkUpdate(NetworkUpdateStage.Initialization);
+                this.RegisterNetworkUpdate(NetworkUpdateStage.FixedUpdate);
+                break;
+            case 4:
+                this.RegisterNetworkUpdate();
+                break;
+        }
+    }
+
+    public void UnregisterUpdates()
+    {
+        this.UnregisterAllNetworkUpdates();
+    }
+
+    private void OnDestroy()
+    {
+        UnregisterUpdates();
+    }
+
+    private readonly int[] m_NetUpdates = new int[7];
+
+    public void NetworkUpdate(NetworkUpdateStage updateStage)
+    {
+        switch (updateStage)
+        {
+            case NetworkUpdateStage.Initialization:
+                m_NetUpdates[0]++;
+                break;
+            case NetworkUpdateStage.EarlyUpdate:
+                m_NetUpdates[1]++;
+                break;
+            case NetworkUpdateStage.FixedUpdate:
+                m_NetUpdates[2]++;
+                break;
+            case NetworkUpdateStage.PreUpdate:
+                m_NetUpdates[3]++;
+                break;
+            case NetworkUpdateStage.Update:
+                m_NetUpdates[4]++;
+                break;
+            case NetworkUpdateStage.PreLateUpdate:
+                m_NetUpdates[5]++;
+                break;
+            case NetworkUpdateStage.PostLateUpdate:
+                m_NetUpdates[6]++;
+                break;
+        }
+    }
+}
---
+++ testproject/Assets/Scripts/NetLoopPerf.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class NetLoopPerf : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            StartCoroutine(Benchmark(128));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            StartCoroutine(Benchmark(1024));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            StartCoroutine(Benchmark(8 * 1024));
+        }
+    }
+
+    private IEnumerator Benchmark(int count)
+    {
+        print($"{nameof(NetLoopPerf)}.{nameof(Benchmark)}(count: {count})");
+
+        var sw = new Stopwatch();
+
+        var gobjs = new List<GameObject>();
+        var comps = new List<NetLoopComp>();
+        for (int i = 0; i < count; i++)
+        {
+            var gobj = new GameObject($"{nameof(NetLoopPerf)}.{nameof(Benchmark)} #{i}");
+            var comp = gobj.AddComponent<NetLoopComp>();
+            gobjs.Add(gobj);
+            comps.Add(comp);
+        }
+
+        yield return new WaitForEndOfFrame();
+
+        sw.Restart();
+        for (int i = 0; i < count; i++)
+        {
+            comps[i].RegisterUpdates(i);
+        }
+
+        sw.Stop();
+        print($"{nameof(NetLoopPerf)}.{nameof(Benchmark)} -> register: {sw.ElapsedMilliseconds}ms");
+
+        yield return new WaitForEndOfFrame();
+
+        sw.Restart();
+        for (int i = 0; i < 8; i++)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        sw.Stop();
+        print($"{nameof(NetLoopPerf)}.{nameof(Benchmark)} -> 8-frames: {sw.ElapsedMilliseconds}ms");
+
+        yield return new WaitForEndOfFrame();
+
+        sw.Restart();
+        for (int i = 0; i < count; i++)
+        {
+            comps[i].UnregisterUpdates();
+        }
+
+        sw.Stop();
+        print($"{nameof(NetLoopPerf)}.{nameof(Benchmark)} -> unregister: {sw.ElapsedMilliseconds}ms");
+
+        yield return new WaitForEndOfFrame();
+
+        for (int i = 0; i < count; i++)
+        {
+            DestroyImmediate(gobjs[i]);
+        }
+
+        gobjs.Clear();
+        comps.Clear();
+    }
+}
